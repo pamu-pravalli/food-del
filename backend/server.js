cae4ae3eb8213bc19bef31d1658da412aa36b8b4
pamu-pravalli <110322859+pamu-pravalli@users.noreply.github.com>
2024-08-10 20:14:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,7 @@ const port=4000;
 
 //middleware
 app.use(express.json())
-app.use(cors({
-    origin: ["https://deploy-mern-1whq.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-}
+app.use(cors(
     ))
 
 //db connection 
