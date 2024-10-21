f60fc74f3118e260fd0f3f35958b777e848eca07
pamu-pravalli <110322859+pamu-pravalli@users.noreply.github.com>
2024-10-21 18:51:04 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = ({setShowLogin}) => {
     <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
     <ul className="navbar-menu">
       <Link to='/' onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link>
-      <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-      <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-      <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+      <a href='#explore-menu' onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</a>
+      <a href='#app-download' onClick={()=>setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Mobile-app</a>
+      <a href='#footer' onClick={()=>setMenu("Contact-us")} className={menu==="Contact-us"?"active":""}>Contact us</a>
     </ul>
     <div className="navbar-right">
         <img src={assets.search_icon} alt=""/>
